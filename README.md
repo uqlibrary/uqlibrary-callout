ad362bbea06782bb5079084c5d48633382a6a839
Jan-Willem Wisgerhof <jwwisgerhof@gmail.com>
2016-03-02 11:01:35 +1000

    First release of uqlibrary-callout

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
 # uqlibrary-callout
+
+A simple element that provides the markup for a UQ Library styled call out. 
+
+## Demo
+
+For a demo and full property rundown see [GH Pages](http://uqlibrary.github.io/uqlibrary-callout).
+
+## Usage
+- Install [npm](https://nodejs.org/en/download/)
+- Install bower
+```sh
+npm install bower -g
+```
+- Install the uqlibrary-callout into your project directory
+```sh
+bower install uqlibrary-callout --save
+```
+- Add the call out anywhere in your page
+```sh
+<uqlibrary-callout json-file="data.json"></uqlibrary-callout>
+```
+- The callout can also be given a json object
+```sh
+HTMLImports.whenReady(function () {
+    var callout = document.querySelector('#callout');
+    callout.menu = {
+        "items": [
+            {
+                "label": "FAQ",
+                "link": "http://faq.com"
+            }
+        ],
+        "summary": {
+            "label": "View more contact options",
+            "link": "http://library.uq.edu.au"
+        }
+    }
+});
+```
+
+## Special Options
+- 
---
+++ elements/uqlibrary-callout.js
@@ -0,0 +1,154 @@
+(function () {
+  Polymer({
+    is: 'uqlibrary-callout',
+    properties: {
+      /**
+       * Holds menu item objects for the call out
+       */
+      menuItems: {
+        type: Array,
+        value: [],
+        observer: '_checkDisabledStatus'
+      },
+      /**
+       * Holds the summary for this call out
+       */
+      summary: {
+        type: Object
+      },
+      /**
+       * JSON Menu object
+       */
+      menu: {
+        type: Object,
+        observer: '_menuChanged'
+      },
+      /**
+       * A relative link to the JSON file containing menu items
+       */
+      jsonFile: {
+        type: String
+      },
+      /**
+       * Whether the chat is online
+       */
+      _chatOnline: {
+        type: Boolean,
+        value: false,
+        observer: '_checkDisabledStatus'
+      },
+      /**
+       * Url to check the status of the chat
+       */
+      _chatStatusUrl: {
+        type: String,
+        value: "https://api2.libanswers.com/1.0/chat/widgets/status/1871"
+      }
+    },
+    ready: function () {
+      this._checkChatStatus();
+
+      if (this.jsonFile) {
+        this._fromJSONFile(this.jsonFile);
+      }
+    },
+    /**
+     * Called when the menu object changes
+     * @private
+     */
+    _menuChanged: function () {
+      if (this.menu) {
+        this._parseJSON(this.menu);
+      }
+    },
+    /**
+     * Reads a JSON file and populates the files
+     * @param jsonFile
+     * @private
+     */
+    _fromJSONFile: function (jsonFile) {
+      var self = this;
+
+      var xobj = new XMLHttpRequest();
+      var fileName = this.resolveUrl(jsonFile);
+      xobj.open('GET', fileName, true);
+      xobj.onreadystatechange = function () {
+        if (xobj.readyState == 4 && xobj.status == "200") {
+          self.menu = JSON.parse(xobj.responseText);
+        }
+      };
+      xobj.send(null);
+    },
+    /**
+     * Parses a menu JSON object and sets the menu items
+     * @param json
+     * @private
+     */
+    _parseJSON: function (json) {
+      this.menuItems = json.items;
+      this.summary = json.summary;
+    },
+    /**
+     * Returns the relevant link for this item
+     * @param item
+     * @returns {*}
+     * @private
+     */
+    _link: function (item) {
+      if (item.linkMobile && this._isMobile()) {
+        return item.linkMobile;
+      } else {
+        return item.link;
+      }
+    },
+    /**
+     * Called when any item is clicked. Fires an event and redirects the user to the given link
+     * @param e
+     * @private
+     */
+    _itemClicked: function (e) {
+      this.fire("uqlibrary-callout-link-clicked", e.model.item);
+
+      window.location = this._link(e.model.item);
+    },
+    /**
+     * Returns whether the user is on a mobile device
+     * @returns {boolean}
+     * @private
+     */
+    _isMobile: function () {
+      return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+    },
+    /**
+     *
+     * @private
+     */
+    _checkChatStatus: function () {
+      var self = this;
+
+      var xobj = new XMLHttpRequest();
+      xobj.open('GET', this._chatStatusUrl, true);
+      xobj.onreadystatechange = function () {
+        if (xobj.readyState == 4 && xobj.status == "200") {
+          var json = JSON.parse(xobj.responseText);
+          self._chatOnline = json.online;
+        }
+      };
+      xobj.send(null);
+    },
+    /**
+     * Called when the chat status has changed. Updates all items' disabled status
+     * @private
+     */
+    _checkDisabledStatus: function () {
+      for (var i = 0; i < this.menuItems.length; i++) {
+        var item = this.menuItems[i];
+        if (item.disabled == "chat-offline" && !this._chatOnline) {
+          item.isDisabled = true;
+        } else {
+          item.isDisabled = false;
+        }
+      }
+    }
+  });
+})();
